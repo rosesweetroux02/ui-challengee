698cf494adfaf7002457e39113f66da820e1b6c6
Rose Tebogo Ndhlovu <114318695+rosesweetroux02@users.noreply.github.com>
2023-02-25 11:27:19 +0200

    FINAL COMPLETED ASSESSMENT

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    var name= "Rose Tebogo Ndhlovu";
+    document.getElementById('name').innerHTML = name;
+
+    var bio= "<br> Hi there! My name is Rose and I'm a web developer in training. I'm passionate<br> about all things tech and I've always been fascinated by the way websites are built. <br> I have experience working with HTML, CSS, JavaScript(basics) & Web hosting,<br> and I'm currently learning JavaScript in context & Node.js. I'm currently seeking<br> web development opportunities that will allow me to further develop my skills<br> and contribute to real-world projects. If you're interested in collaborating on a <br> project or hiring a web developer, feel free to get in touch. I'd love to hear from you!";
+    document.getElementById('bio').innerHTML = bio;
+
+    var address= "Address";
+    document.getElementById('address').innerHTML = address;
+
+    var home= " 50 82nd Avenue Street, <br> Zone 3, <br> Seshego, <br> 0742";
+    document.getElementById('home').innerHTML = home;
+    
+
+};
